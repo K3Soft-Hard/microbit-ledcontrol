b16b3ca9898bd73dfac00b8ce43986b37b837f38
K3Soft-Hard <159473672+K3Soft-Hard@users.noreply.github.com>
2024-02-10 17:13:46 +0100

    Update ledcontrol.ts

--- ledcontrol.ts
+++ ledcontrol.ts
@@ -156,8 +156,8 @@ namespace K3LedControl {
         //%ledx.min=0 ledx.max=4 ledy.min=0 ledy.max=4 ledx2.min=0 ledx2.max=4 ledy2.min=0 ledy2.max=4 
         
     export function plotRectangle(ledx: number, ledy: number,ledx2: number,ledy2: number) {
-        let x2 = ledx2 + 1
-        let y2 = ledy2 + 1
+        let x2 = ledx2 - 1
+        let y2 = ledy2 - 1
         for (let index = 0; index <= x2; index++) {
             for (let index2 = 0; index2 <= y2; index2++) {
                 led.plot(ledx + index, ledy + index2)
